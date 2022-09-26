8076345ea7d54130484d93e83d49134306da15b4
ChristianADiaz <111998383+ChristianADiaz@users.noreply.github.com>
2022-09-26 10:37:37 -0500

    Update 1106.c

--- 1106.c
+++ 1106.c
@@ -2,11 +2,20 @@
 int main() {
 uint8_t a;
 uint8_t b = 6;
-uint8_t c;
 printf ("a a==b a<b a>b");
 for (a = 0; a < 512; a++) {
-  if 
-printf ("%d" a, ");
+printf ("%d %d %d %d", a, a==b, a<b, a>b );
 }
 return 0;
+}
+
+#include <stdio.h>
+int main() {
+uint8_t a;
+uint8_t b = 6;
+printf ("a a==b a<b a>b");
+for (int i = 0; i < 32; i++) {
+  [a = (a+1)% 16]
+printf ("%d %d %d %d", a, a==b, a<b, a>b );
+  return 0;
 }
