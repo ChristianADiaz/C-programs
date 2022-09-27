d987ec7bca7ef03c0fb4598c9f64c0974903b640
ChristianADiaz <111998383+ChristianADiaz@users.noreply.github.com>
2022-09-26 22:25:27 -0500

    Update 1106.c

--- 1106.c
+++ 1106.c
@@ -10,12 +10,13 @@ return 0;
 }
 
 #include <stdio.h>
+#include <stint.h>
 int main() {
 uint8_t a;
 uint8_t b = 6;
-printf ("a a==b a<b a>b");
+printf ("a a==b a<b a>b\n");
 for (int i = 0; i < 32; i++) {
   a = (a+1)% 16;
-printf ("%d %d %d %d", a, a==b, a<b, a>b );
+printf ("%d %d %d %d\n", a, a==b, a<b, a>b );
   return 0;
 }
