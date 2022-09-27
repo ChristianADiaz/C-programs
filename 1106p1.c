d415bd648e9daa258026cef6816bf13c76789f5d
ChristianADiaz <111998383+ChristianADiaz@users.noreply.github.com>
2022-09-26 22:06:25 -0500

    Update 1106p1.c

--- 1106p1.c
+++ 1106p1.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main() {
+uint8_t a;
+uint8_t b = 6;
+printf ("a a==b a<b a>b");
+for (a = 0; a < 512; a++) {
+printf ("%d %d %d %d", a, a==b, a<b, a>b );
+}
+return 0;
+}
