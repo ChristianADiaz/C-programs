daa1a18e024057b10024fa0af313ec6f9c1bdb28
ChristianADiaz <111998383+ChristianADiaz@users.noreply.github.com>
2022-09-26 22:19:22 -0500

    Update 1106p1.c

--- 1106p1.c
+++ 1106p1.c
@@ -5,7 +5,7 @@ uint8_t a;
 uint8_t b = 6;
 printf ("a a==b a<b a>b\n");
 for (a = 0; a < 10; a++) {
-printf ("%d %d %d %d", a, a==b, a<b, a>b );
+printf ("%d %d %d %d\n", a, a==b, a<b, a>b );
 }
 return 0;
 }
